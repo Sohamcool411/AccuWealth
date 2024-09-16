d37c564a68cdcd66bff18bb4da1066a12a4fb5ba
soham <sohamcool411@gmail.com>
2024-09-16 05:59:20 +0530

    pakka last

--- components/Sidebar.tsx
+++ components/Sidebar.tsx
@@ -22,7 +22,7 @@ const Sidebar = ({ user }: SiderbarProps) => {
             alt="Horizon logo"
             className="size-[24px] max-xl:size-14"
           />
-          <h1 className="sidebar-logo">Horizon</h1>
+          <h1 className="sidebar-logo">AccuWealth</h1>
         </Link>
 
         {sidebarLinks.map((item) => {
